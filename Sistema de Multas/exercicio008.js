44f9394167cec0376b9b0a1e68ec35fdf3925721
Werlem Gonçalves da Costa <73136426+Werlem1@users.noreply.github.com>
2022-10-15 11:42:35 -0300

    Sistema de Controle de Velocidade - Multa

---
+++ Sistema de Multas/exercicio008.js
@@ -0,0 +1,8 @@
+console.log('O console funcionou corretamente') //Pressione f8 para ver se está funcionando.
+
+var vel = 62
+console.log(`a velocidade do seu veículo é ${vel}km/h`)
+if (vel > 60) { // Condição Simples
+    console.log('Você ultrapassou a velocidade de 60 km/h. Multado!')
+}
+console.log('Dirija usando sempre cinto de segurança')
---
+++ Sistema de Multas/exercicio009.js
@@ -0,0 +1,10 @@
+//Condiçao Composta
+
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil') { // se. Ou posso usar o diferente !=.
+    console.log('Você é Brasileiro!')
+} else { // senao
+    console.log('Você é Estrangeiro!')
+}
